cfeb09556121786f34404fd6f5adb6767591fbfc
Aya Alsakkaf <aya.alsakkaf@ccse.ku.edu.kw>
2025-03-01 20:59:54 +0300

    structual directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { UserComponent } from './components/user/user.component';
 import { DUMMY_USERS } from '../data/users';
 import { TasksComponent } from "./components/tasks/tasks.component";
+import { NgFor, NgIf } from '@angular/common';
+
 
 @Component({ //decorator, ts feature , which adds meta data to the component
   selector: 'app-root', //the tag that the component will render in it's place
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   standalone: true,
   templateUrl: './app.component.html', //contains the markup or look 
   styleUrl: './app.component.css', //styling for this component. 
